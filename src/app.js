250fe134dedcb0ce6fc3c80091b386d63a823135
evgeniia-korolova <126691505+evgeniia-korolova@users.noreply.github.com>
2023-03-17 00:31:46 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -21,7 +21,8 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -49,12 +50,18 @@ function displayForecast() {
             `; 
   })
   
-  
-
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
-  }
+  //console.log(forecastHTML);
+}
+  
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = '0ebc654fccbc00189d5408f3d6f15b08'; 
+ let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
 
 
 function displayTemperature(response) {
@@ -78,7 +85,9 @@ function displayTemperature(response) {
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
     iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-    iconElement.setAttribute('alt', response.data.weather[0].description);
+  iconElement.setAttribute('alt', response.data.weather[0].description);
+  
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -123,4 +132,4 @@ let celsiusLink = document.querySelector('#celsius-link');
 celsiusLink.addEventListener('click', displayCelsiusTemperature);
 
 search("Odesa");
-displayForecast();
+//displayForecast();
